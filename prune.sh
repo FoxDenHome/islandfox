4b4d1b07cad0fae3a64dc6d814c59b2d774acec2
Doridian <git@doridian.net>
2022-07-11 13:19:21 -0700

    rest-server

--- prune.sh
+++ prune.sh
@@ -5,6 +5,6 @@ runx() {
     /opt/backup/prune.sh
 }
 
-runbackup 'b2:islandfox-backups:/'
+runx 'b2:islandfox-backups:/'
 REST_SERVER_PASSWORD="$(cat /mnt/keydisk/rest-server-password | tr -d '\r\n\t ')"
 runx "rest:https://islandfox:$REST_SERVER_PASSWORD@icefox.doridian.net:8000/islandfox/main"
