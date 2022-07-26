43d374ae8832119c1cef59579d202d990c9ff4dc
Doridian <git@doridian.net>
2022-07-26 16:51:00 -0700

    a

--- gocryptfs-nas.sh
+++ gocryptfs-nas.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-gocryptfs --passfile /mnt/keydisk/gocryptfs-icefox-nas /mnt/nas/zhdd /mnt/nas/zhdd_dec
-gocryptfs --passfile /mnt/keydisk/gocryptfs-icefox-nas /mnt/nas/zssd /mnt/nas/zssd_dec
+gocryptfs --allow_other --passfile /mnt/keydisk/gocryptfs-icefox-nas /mnt/nas/zhdd /mnt/nas/zhdd_dec
+gocryptfs --allow_other --passfile /mnt/keydisk/gocryptfs-icefox-nas /mnt/nas/zssd /mnt/nas/zssd_dec
