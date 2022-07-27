ac6498f4a1ec7eaf781f05fe2f49b11579cba553
Doridian <git@doridian.net>
2022-07-27 09:21:39 -0700

    use this

--- gocryptfs-nas.sh
+++ gocryptfs-nas.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-set -ex
+set -e
 
-#gocryptfs --allow_other --passfile /mnt/keydisk/gocryptfs-icefox-nas /mnt/nas/zhdd /mnt/nas/zhdd_dec
-#gocryptfs --allow_other --passfile /mnt/keydisk/gocryptfs-icefox-nas /mnt/nas/zssd /mnt/nas/zssd_dec
+umount /mnt/nas/* || true
 LC_GOCRYPTFS_PASSWD="$(cat /mnt/keydisk/gocryptfs-icefox-nas)" ssh root@icefox.doridian.net
+mount /mnt/nas/zhdd
+mount /mnt/nas/zssd
+
+docker restart nas2_samba_1 nas2_nginx_1 || true
