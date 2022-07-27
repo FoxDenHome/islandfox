b087046db5b2a2aa0dcd17a1ff2a9e9ad3a21e29
Doridian <git@doridian.net>
2022-07-26 18:59:50 -0700

    rm useless

--- gocryptfs-nas.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -ex
-
-gocryptfs --allow_other --passfile /mnt/keydisk/gocryptfs-icefox-nas /mnt/nas/zhdd /mnt/nas/zhdd_dec
-gocryptfs --allow_other --passfile /mnt/keydisk/gocryptfs-icefox-nas /mnt/nas/zssd /mnt/nas/zssd_dec
