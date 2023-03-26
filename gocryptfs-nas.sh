adf0892fbc86c04cddc6e6f376823e8bb7663dd3
Doridian <git@doridian.net>
2023-03-26 10:28:46 -0700

    rm zssd

--- gocryptfs-nas.sh
+++ gocryptfs-nas.sh
@@ -2,8 +2,4 @@
 
 set -e
 
-umount /mnt/nas/* || true
 LC_GOCRYPTFS_PASSWD="$(cat /mnt/keydisk/gocryptfs-icefox-nas)" ssh root@icefox.doridian.net
-mount /mnt/nas/zhdd
-
-docker restart nas2_samba_1 nas2_nginx_1 || true
